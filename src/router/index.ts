3b712cbee5190559c0c2350c1eb224adc9706c58
devmustbeawesome <programm.rechner@gmail.com>
2024-02-22 10:29:34 +0400

    prepare to start work

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import MainViewVue from '../views/MainView.vue'
+import HomeViewVue from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'main',
-      component: MainViewVue
+      component: HomeViewVue
     }
   ]
 })
