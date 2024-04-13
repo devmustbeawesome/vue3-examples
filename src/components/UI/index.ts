ebd5cbd4a4244cc37d80a9c10bcedad0c301056f
onton <programm.rechner@gmail.com>
2024-04-14 02:13:41 +0400

    rename component

--- src/components/UI/index.ts
+++ src/components/UI/index.ts
@@ -2,5 +2,5 @@ import VButton from '@/components/UI/VButton.vue'
 import VInput from '@/components/UI/VInput.vue'
 import VGrid from '@/components/UI/VGrid.vue'
 import VModal from '@/components/UI/VModal.vue'
-import VDropDownButton from '@/components/UI/VDropDownButton.vue'
-export default [VInput, VButton, VGrid, VModal, VDropDownButton]
+import VDropDown from '@/components/UI/VDropDown.vue'
+export default [VInput, VButton, VGrid, VModal, VDropDown]
